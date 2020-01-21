60a2af0fc500c5dbfc16b8b2f174636889b9993b
Curso <Curso@Ipartek-TOSH03.ipartekaula.com>
2020-01-21 13:04:43 +0100

    servicio rest a mano

---
+++ src/main/java/com/ipartek/formacion/supermercado/controller/ProductoRestController.java
@@ -0,0 +1,102 @@
+package com.ipartek.formacion.supermercado.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.ipartek.formacion.supermercado.modelo.dao.ProductoDAO;
+import com.ipartek.formacion.supermercado.modelo.pojo.Producto;
+
+/**
+ * Servlet implementation class ProductoRestController
+ */
+@WebServlet({ "/producto/", "/producto" })
+public class ProductoRestController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private final static Logger LOG = Logger.getLogger(ProductoRestController.class);
+	
+	private ProductoDAO productoDao;
+	
+	
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		productoDao = ProductoDAO.getInstance();
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		productoDao = null;
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		super.service(request, response); //llama a doGet, doPost, doPut o doDelete
+		
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		LOG.trace("peticion GET");
+		
+		//recuperamos todos los productos de la bd:
+		ArrayList<Producto> lista = (ArrayList<Producto>) productoDao.getAll();
+		
+		//preparamos la respuesta indicando qué tipo de dato devuelve:
+		response.setContentType("application/json"); //por defecto --> text/html;charset=UTF-8
+		response.setCharacterEncoding("utf-8");
+		
+		//response body
+		PrintWriter out = response.getWriter(); //se encarga de poder escribir los datos en el body
+		String jsonResponseBody = new Gson().toJson(lista); //convertir java -> json (usando la librería gson)
+		out.print(jsonResponseBody.toString()); //retornamos un array vacío en json dentro del body
+		out.flush(); //termina de escribir los datos en el body
+		
+		//response status code:
+		response.setStatus( HttpServletResponse.SC_OK ); //200
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<h1>Documentación</h1>
+
+<h2>Productos</h2>
+
+<ul>
+	<li>Listado productos <a href="http://localhost:8080/supermercado-rest/producto/">GET /producto/</a> </li>
+	<li>Detalle producto <a href="http://localhost:8080/supermercado-rest/producto/1">GET /producto/{id}</a> </li>
+</ul>
+
+<p>* El resto de métodos DELETE, POST y PUT hay que probarlos desde JavaScript o POSTMAN </p>
+
